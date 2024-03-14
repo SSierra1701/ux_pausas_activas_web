2a1350215e1318446f4c109cdfb3689dd29bfaa2
Santiago Sierra R <s.sierrar2@uniandes.edu.co>
2024-03-13 21:07:17 -0500

    Feature/Se añade componente login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  showModal: boolean = false;
 
+  constructor(private router: Router) { }
+  
+  onClick(provider: string) {
+    if(provider=='atras'){
+      this.router.navigate(['']);
+    } else{
+      this.router.navigate(['']);
+    }
+  }
+
+  hideModal() {
+    this.showModal = false;
+  }
 }
